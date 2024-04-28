84cc7374f50d72d054c37105ae90f15be79fe007
mtomardev <120013813+mtomardev@users.noreply.github.com>
2024-04-28 16:06:12 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import Search from "./Search";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Search/>
     </div>
   )
 }
---
+++ src/components/Search.js
@@ -0,0 +1,10 @@
+const Search =() =>{
+    return(
+        <div>
+            <h1>Search Movies</h1>
+            <input/>
+            <button>Search</button>
+        </div>
+    )
+}
+export default Search
